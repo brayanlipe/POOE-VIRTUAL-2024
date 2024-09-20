8e6bb1641014c4145936314437b8c1df27724c5c
brayan <you@example.com>
2024-09-19 23:21:36 -0600

    suma de numeros

--- PrimerCodigo/PrimerCodigo/Module1.vb
+++ PrimerCodigo/PrimerCodigo/Module1.vb
@@ -1,8 +1,18 @@
 ﻿Module Module1
 
     Sub Main()
-        Console.WriteLine("hola mundo y todos ")
+        Console.WriteLine("ingrese num1")
+        Dim num1 As Double
+        num1 = Console.ReadLine
 
+        Console.WriteLine("ingrese el num2")
+        Dim num2 As Double
+        num2 = Console.ReadLine
+
+        Console.WriteLine("la suma de los dos numero es: " & num1 + num2)
+
+
+        Console.ReadKey() 'pausa a consola de windows'
 
     End Sub
 
