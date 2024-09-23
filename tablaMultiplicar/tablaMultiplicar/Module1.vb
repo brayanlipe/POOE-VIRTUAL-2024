bcbf8bd2cf9b581a098ff422ea2dc9405a2c1234
brayan <you@example.com>
2024-09-22 21:44:36 -0600

    tabla de multiplicar

---
+++ tablaMultiplicar/tablaMultiplicar/Module1.vb
@@ -0,0 +1,18 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.Write("Ingrese un número: ")
+        Dim numero As Integer = Convert.ToInt32(Console.ReadLine())
+
+        ' Generar y mostrar la tabla de multiplicar
+        For i As Integer = 1 To 10
+            Console.WriteLine(numero & " x " & i & " = " & (numero * i))
+        Next
+
+        ' Esperar a que el usuario presione una tecla para cerrar el programa
+        Console.WriteLine("Presione cualquier tecla para salir...")
+        Console.ReadKey()
+
+    End Sub
+
+End Module
