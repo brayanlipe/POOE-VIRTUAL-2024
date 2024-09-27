db7492c6dacc1f0c44c51e97d24420be5cb0c24a
brayan <you@example.com>
2024-09-27 17:13:09 -0600

    codigo bloque 2 clases

---
+++ pares/pares/Module1.vb
@@ -0,0 +1,36 @@
+﻿Module Module1
+
+    Sub Main()
+        'numero para o impar'
+        Dim numero As Double
+        Dim continuar = "s"
+
+        While continuar = "s"
+
+
+
+            Console.WriteLine("ingrese el numero a comparar")
+            numero = Console.ReadLine()
+
+            If numero Mod 2 = 0 Then
+                Console.WriteLine("el numero es ingresado es par ")
+
+
+            Else
+                Console.WriteLine("el numero es impar")
+
+            End If
+
+
+            Console.WriteLine("desea continuar? s/n")
+            continuar = Console.ReadLine()
+
+
+        End While
+
+        Console.ReadKey()
+
+
+    End Sub
+
+End Module
